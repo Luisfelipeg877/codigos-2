b764b84bf3101246df97d611636d98c88bc40b5f
Luisfelipeg877 <luis.jesus.gomes@escola.pr.gov.br>
2025-05-20 13:23:46 +0000

    codigo 2 trimestre

---
+++ ex1.js
@@ -0,0 +1,33 @@
+for(let i=1;i<=5;i++){
+console.log("Número:"+i);
+}
+let contador=3;
+while(contador>0){
+console.log("Tempo restante:"+
+contador);
+contador--;
+}
+console.log("Acabou!")
+for(let i=1;i<=4;i++){
+ if(i%2===0){
+console.log(i+"é par");
+ }else{
+console.log(i+" é imprar");
+ }
+}
+for(let i=1;i<=10;i++){
+if(i===4) {
+break;
+}
+console.log("Contando:"+i);
+}
+for(let i=1;i<=5;i++){
+if(i===3){
+continue;
+}
+console.log("i="+i);
+}
+let x=1;
+while(x>0){
+console.log("Rodando...");
+}
